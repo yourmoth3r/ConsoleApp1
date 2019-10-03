1d2688031dc5dd9a6f9b9baab80bc7e1d204f382
Thomas Evans <5210582@COMPUTING-GLOS.local>
2019-10-03 10:11:13 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullname;
+            string sgreeting;
+            Console.Write("please enter your full name");
+            fullname = Console.ReadLine();
+            switch (fullname)
+            {
+                case (""):
+                    sgreeting = "y"
+            }
+        }
+    }
+}
